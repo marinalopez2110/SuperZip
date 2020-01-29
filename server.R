1f7d65e41ee2ac7d69e13894f1a88e47424e8272
Marina Lopez <marinalopez2110@gmail.com>
2020-01-29 16:27:08 -0500

    Update server.R
    Added 3 regions Territoire guide

--- server.R
+++ server.R
@@ -3,18 +3,34 @@ library(RColorBrewer)
 library(scales)
 library(lattice)
 library(dplyr)
-
-# Leaflet bindings are a bit slow; for now we'll just sample to compensate
-set.seed(100)
-
-
-####Calling allzips HERE
-#zipdata <- allzips[sample.int(nrow(allzips), 10000),] 
+library(geojsonio)
+library(shiny)
+library(sp)
 
 
-# By ordering by centile, we ensure that the (comparatively rare) SuperZIPs
-# will be drawn last and thus be easier to see
-#zipdata <- zipdata[order(zipdata$centile),]
+TG  <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\TG.json", what = "sp") # DesktopOuranos
+OneA <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\OneA.json", what = "sp") #DesktopOuranos
+TwoC <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\TwoC.json", what = "sp") #DesktopOuranos
+ThreeD <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\ThreeD.json", what = "sp") #DesktopOuranos <- geojsonio::geojson_read("C:\\Users\\marlop1\\Documents\\GitHub\\SuperZip\\www\\TwoC.json", what = "sp") #DesktopOuranos
+
+bins <- c(14, 12, 10, 8, 6, 4, 2, 0, -2, -4, -5)
+
+
+pal <- colorBin("Spectral", domain = TG$tg_mean, bins = bins)
+pal1a <- colorBin("Spectral", domain = OneA$tg_mean, bins = bins)
+pal2c <- colorBin("Spectral", domain = TwoC$tg_mean, bins = bins)
+pal3d <- colorBin("Spectral", domain = ThreeD$tg_mean, bins = bins)
+
+# labels <- sprintf("<strong>%s</strong><br/>%g °C", TG$TER_GUIDE, TG$tg_mean) %>% lapply(htmltools::HTML)
+# labels1 <- sprintf("<strong>%s</strong><br/>%g °C", OneA$TER_GUIDE, OneA$tg_mean) %>% lapply(htmltools::HTML)
+# labels2 <- sprintf("<strong>%s</strong><br/>%g °C", TwoC$TER_GUIDE, TwoC$tg_mean) %>% lapply(htmltools::HTML)
+# labels3 <- sprintf("<strong>%s</strong><br/>%g °C", ThreeD$TER_GUIDE, ThreeD$tg_mean) %>% lapply(htmltools::HTML)
+
+labels <- sprintf( TG$TER_GUIDE, TG$tg_mean) 
+labels1 <- sprintf(OneA$TER_GUIDE, OneA$tg_mean) 
+labels2 <- sprintf(TwoC$TER_GUIDE, TwoC$tg_mean) 
+labels3 <- sprintf(ThreeD$TER_GUIDE, ThreeD$tg_mean)
+
 
 function(input, output, session) {
   
@@ -22,167 +38,68 @@ function(input, output, session) {
   
   # Create the map
   output$map <- renderLeaflet({
-    leaflet() %>%
+    leaflet(OneA) %>%
       addTiles(
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
-      setView(lat = 45.6, lng= -70.5, zoom = 7)
+      setView(lat = 45.6, lng= -70.5, zoom = 7)%>%
+      addPolygons (
+        fillColor = pal1a(OneA$tg_mean),
+        data = OneA,
+        weight = 1,
+        opacity = 1,
+        color = "black",
+        dashArray = "3",
+        fillOpacity = 0.8,
+        label = labels1,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"))%>%
+      addPolygons (
+        fillColor = pal2c(TwoC$tg_mean),
+        data =  TwoC,
+        weight = 1,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(
+          weight = 2,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.8,
+          bringToFront = TRUE),
+        label = labels2,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"))%>%
+      addPolygons (
+        fillColor = pal3d(ThreeD$tg_mean),
+        data =  ThreeD,
+        weight = 1,
+        opacity = 1,
+        color = "red",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(
+          weight = 2,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.8,
+          bringToFront = TRUE),
+        label = labels3,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"))%>%
+      addLegend(pal = pal, values = ~TG$tg_mean, opacity = 0.7, title = NULL,
+                position = "bottomright")
   })
   
-  # A reactive expression that returns the set of zips that are
-  # in bounds right now
-  zipsInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(zipdata[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
-    
-    # subset(zipdata,
-    #        latitude >= latRng[1] & latitude <= latRng[2] &
-    #          longitude >= lngRng[1] & longitude <= lngRng[2])
-  })
   
-  # Precalculate the breaks we'll need for the two histograms
- # centileBreaks <- hist(plot = FALSE, allzips$centile, breaks = 20)$breaks
   
-  # output$histCentile <- renderPlot({
-  #   # If no zipcodes are in view, don't plot
-  #   if (nrow(zipsInBounds()) == 0)
-  #     return(NULL)
-  #   
-  #   hist(zipsInBounds()$centile,
-  #        breaks = centileBreaks,
-  #        main = "SuperZIP score (visible zips)",
-  #        xlab = "Percentile",
-  #        xlim = range(allzips$centile),
-  #        col = '#00DD00',
-  #        border = 'white')
-  # })
-  
-  # output$scatterCollegeIncome <- renderPlot({
-  #   # If no zipcodes are in view, don't plot
-  #   if (nrow(zipsInBounds()) == 0)
-  #     return(NULL)
-  #   
-  #   print(xyplot(income ~ college, data = zipsInBounds(), xlim = range(allzips$college), ylim = range(allzips$income)))
-  # })
-  
-  # This observer is responsible for maintaining the circles and legend,
-  # according to the variables the user has chosen to map to color and size.
-  # observe({
-  #   colorBy <- input$color
-  #   sizeBy <- input$size
-  #   
-  #   if (colorBy == "superzip") {
-  #     # Color and palette are treated specially in the "superzip" case, because
-  #     # the values are categorical instead of continuous.
-  #     colorData <- ifelse(zipdata$centile >= (100 - input$threshold), "yes", "no")
-  #     pal <- colorFactor("viridis", colorData)
-  #   } else {
-  #     colorData <- zipdata[[colorBy]]
-  #     pal <- colorBin("viridis", colorData, 7, pretty = FALSE)
-  #   }
-  #   
-  #   if (sizeBy == "superzip") {
-  #     # Radius is treated specially in the "superzip" case.
-  #     radius <- ifelse(zipdata$centile >= (100 - input$threshold), 30000, 3000)
-  #   } else {
-  #     radius <- zipdata[[sizeBy]] / max(zipdata[[sizeBy]]) * 30000
-  #   }
-  #   
-  #   leafletProxy("map", data = zipdata) %>%
-  #     clearShapes() %>%
-  #     addCircles(~longitude, ~latitude, radius=radius, layerId=~zipcode,
-  #                stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>%
-  #     addLegend("bottomleft", pal=pal, values=colorData, title=colorBy,
-  #               layerId="colorLegend")
-  # })
-  
-  # Show a popup at the given location
-  # showZipcodePopup <- function(zipcode, lat, lng) {
-  #   selectedZip <- allzips[allzips$zipcode == zipcode,]
-  #   content <- as.character(tagList(
-  #     tags$h4("Score:", as.integer(selectedZip$centile)),
-  #     tags$strong(HTML(sprintf("%s, %s %s",
-  #                              selectedZip$city.x, selectedZip$state.x, selectedZip$zipcode
-  #     ))), tags$br(),
-  #     sprintf("Median household income: %s", dollar(selectedZip$income * 1000)), tags$br(),
-  #     sprintf("Percent of adults with BA: %s%%", as.integer(selectedZip$college)), tags$br(),
-  #     sprintf("Adult population: %s", selectedZip$adultpop)
-  #   ))
-  #   leafletProxy("map") %>% addPopups(lng, lat, content, layerId = zipcode)
-  # }
-  
-  # When map is clicked, show a popup with city info
-  # observe({
-  #   leafletProxy("map") %>% clearPopups()
-  #   event <- input$map_shape_click
-  #   if (is.null(event))
-  #     return()
-  #   
-  #   isolate({
-  #     showZipcodePopup(event$id, event$lat, event$lng)
-  #   })
-  # })
-  # 
-  
-  ## Data Explorer ###########################################
-  
-  # observe({
-  #   cities <- if (is.null(input$states)) character(0) else {
-  #     filter(cleantable, State %in% input$states) %>%
-  #       `$`('City') %>%
-  #       unique() %>%
-  #       sort()
-  #   }
-  #   stillSelected <- isolate(input$cities[input$cities %in% cities])
-  #   updateSelectizeInput(session, "cities", choices = cities,
-  #                        selected = stillSelected, server = TRUE)
-  # })
-  # 
-  # observe({
-  #   zipcodes <- if (is.null(input$states)) character(0) else {
-  #     cleantable %>%
-  #       filter(State %in% input$states,
-  #              is.null(input$cities) | City %in% input$cities) %>%
-  #       `$`('Zipcode') %>%
-  #       unique() %>%
-  #       sort()
-  #   }
-  #   stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
-  #   updateSelectizeInput(session, "zipcodes", choices = zipcodes,
-  #                        selected = stillSelected, server = TRUE)
-  # })
-  
-  # observe({
-  #   if (is.null(input$goto))
-  #     return()
-  #   isolate({
-  #     map <- leafletProxy("map")
-  #     map %>% clearPopups()
-  #     dist <- 0.5
-  #     zip <- input$goto$zip
-  #     lat <- input$goto$lat
-  #     lng <- input$goto$lng
-  #     showZipcodePopup(zip, lat, lng)
-  #     map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
-  #   })
-  # })
-  # 
-  # output$ziptable <- DT::renderDataTable({
-  #   df <- cleantable %>%
-  #     filter(
-  #       Score >= input$minScore,
-  #       Score <= input$maxScore,
-  #       is.null(input$states) | State %in% input$states,
-  #       is.null(input$cities) | City %in% input$cities,
-  #       is.null(input$zipcodes) | Zipcode %in% input$zipcodes
-  #     ) %>%
-  #     mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
-  #   action <- DT::dataTableAjax(session, df, outputId = "ziptable")
-  #   
-  #   DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
-  # })
+
 }
