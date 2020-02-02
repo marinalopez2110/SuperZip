e77f02b9dd0ee8f7a062f74b8f203327afc04a50
Marina Lopez <marinalopez2110@gmail.com>
2020-02-02 09:57:42 -0500

    Update server.R
    Code cleanup

--- server.R
+++ server.R
@@ -14,13 +14,8 @@ load_json <- function (region){
   print(fname)
   geojsonio::geojson_read(fname, what = "sp")
 }
-#TG  <- geojsonio::geojson_read("www/TG.json", what = "sp") # Deployment
-# t1a <- geojsonio::geojson_read("www\\1a.json", what = "sp") #Deployment
-# t2c <- geojsonio::geojson_read("www/2c.json", what = "sp") #Deployment
-# t3d <- geojsonio::geojson_read("www/3d.json", what = "sp") #Deployment
 
-#inputST <- reactive(paste(t,input$Territoires))
-
+TG1  <- geojsonio::geojson_read("www/TG.json", what = "sp")
 
 bins <- c('-5', '-4', '-2', '0', '2', '4', '6', '8', '10', '12', '14' )
 
@@ -29,76 +24,9 @@ palette <- function(inputST){
   return (pal)
 }
 
-# pal <- colorBin("Spectral", domain = TG$tg_mean, bins = bins)
-# pal1a <- colorBin("Spectral", domain = OneA$tg_mean, bins = bins)
-# pal2c <- colorBin("Spectral", domain = TwoC$tg_mean, bins = bins)
-# pal3d <- colorBin("Spectral", domain = ThreeD$tg_mean, bins = bins)
-
-# labels <- sprintf("<strong>%s</strong><br/>%g °C", TG$TER_GUIDE, TG$tg_mean) %>% lapply(htmltools::HTML)
-#labels <- as.character(tagList(tags$strong(HTML(sprintf("Region: %s", TG$TER_GUIDE))), tags$br(), sprintf("Temp: %s", TG$tg_mean)))
-#labels  <- sprintf("Région: %s - Temp: %s", TG$TER_GUIDE, TG$tg_mean)
-#labels <- sprintf("Région: %s - Temp: %s", inputST$TER_GUIDE, inputST$tg_mean) 
-#labels2 <- sprintf("Région: %s - Temp: %s", TwoC$TER_GUIDE, TwoC$tg_mean) 
-#labels3 <- sprintf("Région: %s - Temp: %s", ThreeD$TER_GUIDE, ThreeD$tg_mean)
-
-
 function(input, output, session) {
   
- 
   ## Interactive Map ###########################################
-  
-  # This observer is responsible for plotting temperature shapes,
-  # according to the variables the user has chosen to map to color and size.
-  observe({
-    TG <- input$Territoires
-    dataTG =  load_json(TG)
-    #if (TG == '1a') {
-      # Color and palette are treated specially in the "superzip" case, because
-      # the values are categorical instead of continuous.
-    pal <- palette(dataTG)
-    
-    leafletProxy("map", data = dataTG) %>%
-      
-        #clearShapes() %>%
-      addPolygons (
-            fillColor = pal(dataTG$tg_mean),
-            data = dataTG, #OneA,
-            weight = 1,
-            opacity = 1,
-            color = "black",
-            dashArray = "3",
-            fillOpacity = 0.8,
-            # label = labels,
-            # labelOptions = labelOptions(
-            #   style = list("font-weight" = "normal", padding = "3px 8px"),
-            #   textsize = "15px",
-            #   direction = "auto")
-            )
-    # } else if (TG == '2c') {
-    # leafletProxy("map", data = TwoC) %>%
-    #  # clearShapes() %>%
-    # addPolygons (
-    #       fillColor = pal2c(TwoC$tg_mean),
-    #       data =  TwoC,
-    #       weight = 1,
-    #       opacity = 1,
-    #       color = "white",
-    #       dashArray = "3",
-    #       fillOpacity = 0.7,
-    #       highlight = highlightOptions(
-    #         weight = 2,
-    #         color = "#666",
-    #         dashArray = "",
-    #         fillOpacity = 0.8,
-    #         bringToFront = TRUE),
-    #       label = labels2,
-    #       labelOptions = labelOptions(
-    #         style = list("font-weight" = "normal", padding = "3px 8px"),
-    #         textsize = "15px",
-    #         direction = "auto"))
- # }
-  })
-  
   # Create the map
   output$map <- renderLeaflet({
     leaflet(map) %>%
@@ -109,98 +37,33 @@ function(input, output, session) {
       setView(lat = 45.6, lng= -70.5, zoom = 7)
   })
   
-  # observe({
-  #   TG2 <- input$Territoires2
-  #   
-  #   if (TG2 == '1a') {
-  #     # Color and palette are treated specially in the "superzip" case, because
-  #     # the values are categorical instead of continuous.
-  #     leafletProxy("map", data = OneA) %>%
-  #       #clearShapes() %>%
-  #       addPolygons (
-  #         fillColor = pal1a(OneA$tg_mean),
-  #         data = OneA,
-  #         weight = 1,
-  #         opacity = 1,
-  #         color = "black",
-  #         dashArray = "3",
-  #         fillOpacity = 0.8,
-  #         label = labels1,
-  #         labelOptions = labelOptions(
-  #           style = list("font-weight" = "normal", padding = "3px 8px"),
-  #           textsize = "15px",
-  #           direction = "auto"))
-  #   } else if (TG2 == '2c') {
-  #     leafletProxy("map", data = TwoC) %>%
-  #       #clearShapes() %>%
-  #       addPolygons (
-  #         fillColor = pal2c(TwoC$tg_mean),
-  #         data =  TwoC,
-  #         weight = 1,
-  #         opacity = 1,
-  #         color = "white",
-  #         dashArray = "3",
-  #         fillOpacity = 0.7,
-  #         highlight = highlightOptions(
-  #           weight = 2,
-  #           color = "#666",
-  #           dashArray = "",
-  #           fillOpacity = 0.8,
-  #           bringToFront = TRUE),
-  #         label = labels2,
-  #         labelOptions = labelOptions(
-  #           style = list("font-weight" = "normal", padding = "3px 8px"),
-  #           textsize = "15px",
-  #           direction = "auto"))
-  #   }
-  # })
-  
-  
-  # observe({
-  #   TG3 <- input$Territoires3
-  #   
-  #   if (TG3 == '1a') {
-  #     # Color and palette are treated specially in the "superzip" case, because
-  #     # the values are categorical instead of continuous.
-  #     leafletProxy("map", data = OneA) %>%
-  #       #clearShapes() %>%
-  #       addPolygons (
-  #         fillColor = pal1a(OneA$tg_mean),
-  #         data = OneA,
-  #         weight = 1,
-  #         opacity = 1,
-  #         color = "black",
-  #         dashArray = "3",
-  #         fillOpacity = 0.8,
-  #         label = labels1,
-  #         labelOptions = labelOptions(
-  #           style = list("font-weight" = "normal", padding = "3px 8px"),
-  #           textsize = "15px",
-  #           direction = "auto"))
-  #   } else if (TG3 == '2c') {
-  #     leafletProxy("map", data = TwoC) %>%
-  #       #clearShapes() %>%
-  #       addPolygons (
-  #         fillColor = pal2c(TwoC$tg_mean),
-  #         data =  TwoC,
-  #         weight = 1,
-  #         opacity = 1,
-  #         color = "white",
-  #         dashArray = "3",
-  #         fillOpacity = 0.7,
-  #         highlight = highlightOptions(
-  #           weight = 2,
-  #           color = "#666",
-  #           dashArray = "",
-  #           fillOpacity = 0.8,
-  #           bringToFront = TRUE),
-  #         label = labels2,
-  #         labelOptions = labelOptions(
-  #           style = list("font-weight" = "normal", padding = "3px 8px"),
-  #           textsize = "15px",
-  #           direction = "auto"))
-  #   }
-  # })
-  
+  # This observer is responsible for plotting temperature shapes,
+  # according to the variables the user has chosen to map to color and size.
+  observe({
+    TG <- input$Territoires
+    dataTG =  load_json(TG)
+    pal <- palette(dataTG)
+    labels <- sprintf("Région: %s - Temp: %s", dataTG$TER_GUIDE, dataTG$tg_mean)
+    leafletProxy("map", data = dataTG) %>%
+        clearShapes() %>%
+      addPolygons (
+            fillColor = pal(dataTG$tg_mean),
+            data = dataTG, #OneA,
+            weight = 1,
+            opacity = 1,
+            color = "black",
+            dashArray = "3",
+            fillOpacity = 0.8,
+             label = labels,
+             labelOptions = labelOptions(
+               style = list("font-weight" = "normal", padding = "3px 8px"),
+               textsize = "15px",
+               direction = "auto")
+            ) %>%
+     addLegend(pal = palette, values = ~TG1$tg_mean, title = "Température (°C)", opacity = 0.7,
+               position = "bottomright")
+
+  })
+ 
 
 }
